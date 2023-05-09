fb58d3a81f9fb38be5dd5398dc445a5b9b151500
Theta Rich <10991478@uvu.edu>
2023-05-09 14:07:47 -0600

    Still trying to figure it out - PlayerBottom won't detect collisions at all

--- PlatformerGame/Assets/Scripts/ChildCollisionScript.cs
+++ PlatformerGame/Assets/Scripts/ChildCollisionScript.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class ChildCollisionScript : MonoBehaviour
 {
+    private void Awake()
+    {
+        Debug.Log("Child awoken");
+    }
     private void OnCollisionEnter(Collision other)
     {
+        Debug.Log("Child collided");
         transform.parent.GetComponent<ParentCollisionScript>().CollisionDetected(this, other);
     }
 }
