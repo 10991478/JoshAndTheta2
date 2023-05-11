0a1584881cf10418c7c2bd60e86df12e5b9514e4
Theta Rich <10991478@uvu.edu>
2023-05-11 12:55:40 -0600

    Added moving platforms

--- PlatformerGame/Assets/Scripts/ChildCollisionScript.cs
+++ PlatformerGame/Assets/Scripts/ChildCollisionScript.cs
@@ -3,15 +3,10 @@ using UnityEngine.Tilemaps;
 
 public class ChildCollisionScript : MonoBehaviour
 {
-    private void Awake()
-    {
-        Debug.Log("Child awoken");
-    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other != null)
         {
-            //Debug.Log($"Child collided with {other.gameObject.GetComponent<IDContainer>().id}");
             transform.parent.GetComponent<ParentCollisionScript>().CollisionDetected(this, other);
         }
     }
