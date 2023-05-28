c504c07115c1d7643a90cc67bba34a30e1d962f1
ButWhatIfJosh <josh123car@gmail.com>
2023-05-28 16:41:47 -0600

    enemies
    made a new snowman enemy that for now is pretty much the same as the default.
    changed the move side to side script so that if the platform isn't big enough, when it reaches the edge, it will turn around

--- PlatformerGame/Assets/Scripts/MoveSideToSide.cs
+++ PlatformerGame/Assets/Scripts/MoveSideToSide.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,21 +9,35 @@ public class MoveSideToSide : MonoBehaviour
     public float speed = 3f;
     private float timeOfLastFlip;
     public float flipFrequency = 3f;
+    private float turn = .1f;
     private Rigidbody2D rb;
+    private Collider2D coll;
+    [SerializeField] private GameObject enemy;
+    [SerializeField] private LayerMask platformEdge;
 
     void Start()
     {
         timeOfLastFlip = Time.time;
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
     }
 
     void Update()
     {
-        if (Time.time - timeOfLastFlip >= flipFrequency)
+        if (Time.time - timeOfLastFlip >= flipFrequency || OnEdge())
         {
-            currentDirection *= -1;
-            timeOfLastFlip = Time.time;
+            if (Time.time - timeOfLastFlip >= turn)
+            {
+                currentDirection *= -1;
+                timeOfLastFlip = Time.time;
+            }    
         }
         rb.velocity = new Vector2(speed * currentDirection, rb.velocity.y);
+    }
+
+    private bool OnEdge()
+    {
+        bool onEdge = Physics2D.BoxCast(coll.bounds.center, new Vector2(coll.bounds.size.x*.9f, coll.bounds.size.y*.7f), 0f, Vector2.down, .2f, platformEdge);
+        return onEdge;
     }
 }
